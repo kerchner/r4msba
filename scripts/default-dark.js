6f892c590e41af0ea0042bdfeb4548b91dc78005
kerchner@gwu.edu <kerchner@gwu.edu>
2025-09-04 10:15:39 -0400

    default to dark

---
+++ scripts/default-dark.js
@@ -0,0 +1,9 @@
+<script>
+// scripts/default-dark.js
+(function() {
+  // Only set if user hasn’t chosen before
+  if (!localStorage.getItem("quarto-color-scheme")) {
+    localStorage.setItem("quarto-color-scheme", "dark");
+  }
+})();
+</script>
